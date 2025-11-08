407217780a9b0f0c532ada6f4bcc1686b0cd0025
andremc331 <andremc331@gmail.com>
2025-11-08 09:10:19 -0300

    ajuste rota frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://projeto-abp.onrender.com ", 
+  baseURL: "https://projeto-abp.onrender.com", 
 });
 
 export default api;
